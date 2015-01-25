0144aab79963e55ec123c04a63ae7829bb329f6c
mikehmbn <michael.l.hamilton@wsu.edu>
2015-01-24 20:32:26 -0800

    Adding LED
    Teensy LED will flash 3 times on starting, then once each data
    transmission.

--- Cougballon_Primary_Tx/Cougballon_Primary_Tx.ino
+++ Cougballon_Primary_Tx/Cougballon_Primary_Tx.ino
@@ -29,6 +29,21 @@ void setup() {
   Serial1.println("########################################");
   Serial1.println("");
   Serial1.println("");
+  pinMode(13,OUTPUT);
+  
+  //BLINK THREE TIMES TO SHOW IT'S ON
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
   
 }
 
@@ -62,40 +77,94 @@ void loop() {
   Serial.println("SENT!");
   
   Serial1.println("A1.23");// A IS HUMIDITY
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("B2.34");// B IS CO LEVEL
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("C4.32");// C IS CH4 LEVEL
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("D84.3212312");// D IS RADIO/GPS BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("E87.321231123");// E IS HACKHD1 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("F93.3434523452435");// F IS HACKHD2 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("G75.2462345124531");// G IS HACKHD3 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("I62.123412341");// I IS HACKHD4 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("J98.99999999");// J IS HACKHD5 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("K99.111111111");// K IS HACKHD6 BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("L58.345234");// L IS VIDEO TRANSMITTER BATTERY LEVEL (PERCENTAGE LEFT)
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("HackHD111001"); // H IS HACKHD STATUS
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("P3.45");// P IS INTERNAL PRESSURE
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("Q4.56");// Q IS EXTERNAL PRESSURE
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("T5.67");// T IS INTERNAL TEMPERATURE
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("U6.78");// U IS VIDEO TRANSMITTER TEMPERATURE
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("V7.89");// V IS EXTERNAL TEMPERATURE
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
   Serial1.println("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
   delay(2000);
    
 }
