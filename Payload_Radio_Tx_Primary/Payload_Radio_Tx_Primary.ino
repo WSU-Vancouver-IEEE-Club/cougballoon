22087b154a259186535453865f923e3989f34e52
Mike Hansen <mike@generictech.net>
2015-03-11 17:41:52 -0700

    Minor tweaks
    Minor tweaks.

--- Payload_Radio_Tx_Primary/Payload_Radio_Tx_Primary.ino
+++ Payload_Radio_Tx_Primary/Payload_Radio_Tx_Primary.ino
@@ -17,6 +17,8 @@
 
 //Serial1 TX will be sending data to the radio 
 
+//Serial RXTX is USB debug only.
+
 String stringFromSensors = "";
 String stringFromGPS = "";
 String stringFromHackHDtest = "";
@@ -24,10 +26,10 @@ String stringFromHackHDtest = "";
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(5, 6, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  Serial2.begin(9600);
-  Serial3.begin(9600);
+  Serial.begin(9600);  //Serial RXTX is USB debug only.
+  Serial1.begin(9600); //Serial1 RX will be receiving the HackHD information, Serial1 TX will be sending data to the radio 
+  Serial2.begin(9600); //Serial2 RX will be receiving the GPS Data
+  Serial3.begin(9600); //Serial3 RX will be receiving the Air Quality/Temp Levels
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
@@ -41,38 +43,47 @@ void setup() {
   Serial1.println("");
   pinMode(13, OUTPUT);
 
-  //BLINK THREE TIMES TO SHOW IT'S ON
+  //BLINK FIVE TIMES TO SHOW IT'S ON, NEOPIXELS CYCLE
   digitalWrite(13, HIGH);
-  strip.setPixelColor(0, 127, 0, 0);
+  strip.setPixelColor(0, 127, 0, 0); //RED
   strip.show();
   delay(100);
   digitalWrite(13, LOW);
   delay(100);
+  
   digitalWrite(13, HIGH);
-  strip.setPixelColor(1, 127, 127, 0);
+  strip.setPixelColor(1, 127, 127, 0); //YELLOW
   strip.show();
   delay(100);
   digitalWrite(13, LOW);
   delay(100);
+  
   digitalWrite(13, HIGH);
-  strip.setPixelColor(2, 0, 127, 0);
+  strip.setPixelColor(2, 0, 127, 0); //GREEN
   strip.show();
   delay(100);
   digitalWrite(13, LOW);
   delay(100);
+  
   digitalWrite(13, HIGH);
-  strip.setPixelColor(3, 0, 0, 127);
+  strip.setPixelColor(3, 0, 0, 127); //BLUE
   strip.show();
   delay(100);
   digitalWrite(13, LOW);
   delay(100);
+  
   digitalWrite(13, HIGH);
-  strip.setPixelColor(4, 127, 0, 127);
+  strip.setPixelColor(4, 127, 0, 127); //PURPLE
   strip.show();
   delay(100);
   digitalWrite(13, LOW);
-  delay(100);
-
+  delay(500);
+  
+  for(int i = 0; i < 5; i++) {
+    strip.setPixelColor(i, 0, 0, 0); //OFF
+    strip.show();
+    delay(75);
+  }
 }
 
 void loop() {
@@ -80,33 +91,41 @@ void loop() {
   if (Serial1.available()) {
     incomingData1();
     Serial1.println(stringFromHackHDtest);
-    digitalWrite(13,HIGH);
-    delay(100);
-    digitalWrite(13,LOW);
-    //Serial.println(stringFromHackHDtest);//FOR DEBUGGING
+    strip.setPixelColor(0, 0, 127, 0); //GREEN
+    strip.show();
+    //Serial.println(stringFromHackHDtest); //FOR DEBUGGING
     stringFromHackHDtest = "";
+  }
+  else {
+    strip.setPixelColor(0, 0, 0, 0); //OFF
+    strip.show();
   }
 
   if (Serial2.available()) {
     incomingData2();
     Serial1.println(stringFromGPS);
-    digitalWrite(13,HIGH);
-    delay(100);
-    digitalWrite(13,LOW);
-    //Serial.println(stringFromGPS);//FOR DEBUGGING
+    strip.setPixelColor(1, 0, 127, 0); //GREEN
+    strip.show();
+    //Serial.println(stringFromGPS); //FOR DEBUGGING
     stringFromGPS = "";
+  }
+  else {
+    strip.setPixelColor(1, 0, 0, 0); //OFF
+    strip.show();
   }
 
   if (Serial3.available()) {
     incomingData3();
     Serial1.println(stringFromSensors);
-    digitalWrite(13,HIGH);
-    delay(100);
-    digitalWrite(13,LOW);
-    //Serial.println(stringFromSensors);//FOR DEBUGGING
+    strip.setPixelColor(2, 0, 127, 0); //GREEN
+    strip.show();
+    //Serial.println(stringFromSensors); //FOR DEBUGGING
     stringFromSensors = "";
   }
-
+  else {
+    strip.setPixelColor(2, 0, 0, 0); //OFF
+    strip.show();
+  }
 }
 
 void incomingData1() {
@@ -132,8 +151,3 @@ void incomingData3() {
     delay(5); //give the serial port time to catch up
   }
 }
-
-
-
-
-
